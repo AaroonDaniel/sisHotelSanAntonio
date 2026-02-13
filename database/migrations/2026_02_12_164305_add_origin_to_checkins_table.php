9c66ebef173b260274a08c4122eb52259e056e0e
daniel <cuentauni2022daniel@gmail.com>
2026-02-13 10:58:13 -0400

    adicion de origen en checkin

---
+++ database/migrations/2026_02_12_164305_add_origin_to_checkins_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(){
+        Schema::table('checkins', function (Blueprint $table){
+            $table->string('origin')->nullable()->after('schedule_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('checkins', function (Blueprint $table) {
+            //
+        });
+    }
+};
