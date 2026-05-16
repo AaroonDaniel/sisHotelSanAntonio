679804f62ab32fab0cefc91521b4a067cbe9b07d
daniel <cuentauni2022daniel@gmail.com>
2026-05-16 12:44:00 -0400

    arreglo de campos parte 1

--- routes/settings.php
+++ routes/settings.php
@@ -23,6 +23,4 @@ Route::middleware('auth')->group(function () {
         return Inertia::render('settings/appearance');
     })->name('appearance.edit');
 
-    Route::get('settings/two-factor', [TwoFactorAuthenticationController::class, 'show'])
-        ->name('two-factor.show');
 });
